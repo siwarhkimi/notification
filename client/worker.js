1065926b25ea8e920325a635ee11e4ca7b1c3b65
siwarhkimi <cywarhkimi@gmail.com>
2020-11-03 21:09:14 +0100

    add some text to notif

--- client/worker.js
+++ client/worker.js
@@ -6,7 +6,7 @@ self.addEventListener('push', e => {
 	const data = e.data.json();
 	console.log('Push Recieved...');
 	self.registration.showNotification(data.title, {
-		body: 'Life is wonderful',
+		body: 'Life is wonderful so enjoy it!!',
 		icon: 'https://www.citypng.com/public/uploads/preview/-11591552248eawydg4hnu.png'
 	})
 })
